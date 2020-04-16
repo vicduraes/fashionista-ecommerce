16636f659623a49f7a8df78c6e332934cfd7263d
vicduraes <duraes.vic@gmail.com>
2020-04-16 00:28:35 -0300

    Ajustar rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Routes from './routes';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes />
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Detalhe from './pages/Detalhe/Detalhe';
@@ -7,8 +7,8 @@ import CarrinhoDeCompra from './pages/CarrinhoDeCompra/CarrinhoDeCompra';
 
 export default () => (
   <Switch>
-    <Route path="/" component={Home} />
     <Route path="/detalhe-catalogo" component={Detalhe} />
     <Route path="/carrinho-compra" component={CarrinhoDeCompra} />
+    <Route path="*" component={Home} />
   </Switch>
 );
