81b4337c29c063a757520aec3f58143db9083c9f
vicduraes <duraes.vic@gmail.com>
2020-04-16 00:30:04 -0300

    Criar pasta de styles e configurar variaveis

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
+import './styles/main.css';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
-    return (
-        <h1>HOME</h1>
-    )
-}
+  return <h1>HOME</h1>;
+};
 
 export default Home;
