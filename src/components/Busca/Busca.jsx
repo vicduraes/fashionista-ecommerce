25e80882d00584ccc4c24443f0ed161b48e3e5bc
vicduraes <duraes.vic@gmail.com>
2020-04-24 21:59:11 -0300

    Estilizar componente de busca

--- src/components/Busca/Busca.jsx
+++ src/components/Busca/Busca.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import "./Busca.css";
 
 const Busca = () => {
   return (
-    <>
-      <input type="text" placeholder="Faça sua busca"></input>
-      <FontAwesomeIcon icon={faSearch} />
-    </>
+    <div className="busca">
+      <input
+        type="text"
+        placeholder="O que você procura?"
+        className="busca__input"
+      ></input>
+      <button type="submit" className="busca__botao">
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
+    </div>
   );
 };
 
