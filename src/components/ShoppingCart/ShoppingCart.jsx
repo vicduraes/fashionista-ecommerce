9facc44a90ab04c2cb4ba200c0815532649d80f9
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-03 16:06:07 -0300

    Refatoração: aplica SASS no componente shoppingCart

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import "./ShoppingCart.css";
-
+import "./ShoppingCart.scss";
 
 const ShoppingCart = () => {
   return (
-    <button type="submit" className="navbar__shoppingcart">
+    <button type="submit" className="shoppingcart">
       <FontAwesomeIcon icon={faShoppingCart}/>
-      <i data-count="2" className="navbar__shoppingcart--badge"></i>
+      <i data-count="2" className="shoppingcart--badge"></i>
     </button>
   );
 };
