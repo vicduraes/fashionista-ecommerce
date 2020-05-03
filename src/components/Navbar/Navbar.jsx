eab0d5ee2cd5ce9414d9233253f0aa845a697f05
vicduraes <duraes.vic@gmail.com>
2020-05-03 17:46:04 -0300

    estilo: finaliza ajuste responsividade da navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import ShoppingCart from "../ShoppingCart/ShoppingCart.jsx";
 import Search from "../Search/Search";
-import BotaoVoltar from "../BotaoVoltar/BotaoVoltar";
-import logo from "../../assets/images/logo.png";
-import "./Navbar.css";
+import BackArrowIcon from "../BackArrowIcon/BackArrowIcon";
+import logo from "../../assets/images/logo.svg";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <BotaoVoltar />
-      <img alt="logo da Fashionista" className="navbar__logo" src={logo} />
-      <ShoppingCart />
-      <Search />
-    </div>
+    <>
+      <div className="navbar">
+        <BackArrowIcon />
+        <img alt="logo da Fashionista" className="navbar__logo" src={logo} />
+        <ShoppingCart />
+        <Search />
+      </div>
+      <div className="navbar--desktop">
+        <img alt="logo da Fashionista" className="navbar__logo" src={logo} />
+        <div className="navbar__icons">
+          <Search />
+          <ShoppingCart />
+        </div>
+      </div>
+    </>
   );
 };
 
