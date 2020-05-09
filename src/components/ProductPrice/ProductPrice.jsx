67c444e17e5de3c9e32b74108f42244ed70f3802
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-08 21:45:36 -0300

    funcionalidade: cria componente ProductPrice

--- src/components/ProductPrice/ProductPrice.jsx
+++ src/components/ProductPrice/ProductPrice.jsx
@@ -1,22 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./ProductPrice.scss";
 
-// import "./ProductPrice.scss";
-
-const ProductPrice = ({ price }) => {
-  return (
-    <span className="card__price">
-    {' '}
-    R$
-{price}
-    {' '}
-         </span>;
-};
+const ProductPrice = ({ price, size }) => {
+  var className = 'product__price--' + size ;
+  return <span className={className}> R${price}</span>
+  };
 
 ProductPrice.propTypes = {
   price: PropTypes.number.isRequired,
+  size:  PropTypes.oneOf(['small', 'medium', 'large']),
 };
 
 export default ProductPrice;
-ProductPrice.propTypes = {
-  price: PropTypes.number.isRequired,
-};
+
