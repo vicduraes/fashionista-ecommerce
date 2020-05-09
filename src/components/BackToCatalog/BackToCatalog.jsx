7ac2abf288cdaa755948d830b742a65b38751d33
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-05-09 09:07:09 -0300

    finaliza componente de voltar ao catálogo

--- src/components/BackToCatalog/BackToCatalog.jsx
+++ src/components/BackToCatalog/BackToCatalog.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './BackToCatalog.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom'
 
 const BackToCatalog = (props) => {
   return (
-    <div className="back-to-catalog">
-      <FontAwesomeIcon className="back-to-catalog__icon" icon={faChevronLeft} />
-      <p className="back-to-catalog__text">Voltar ao Catálogo</p>
-    </div>
+    <Link to='/' style={{ textDecoration: 'none' }}>
+        <div className="back-to-catalog" >
+        <FontAwesomeIcon className="back-to-catalog__icon" icon={faChevronLeft} />
+        <p className="back-to-catalog__text">Voltar ao Catálogo</p>
+        </div>
+    </Link> 
   );
 };
 
