b02203ee8279e63196eeaf261454513fa329475d
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-08 23:55:25 -0300

    WIP feature: cria página detalhe de produto

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import ChooseSize from "../../components/ChooseSize/ChooseSize";
 import AddPack from "../../components/AddPack/AddPack";
+import ProductName from "../../components/ProductName/ProductName";
+import ProductPrice from "../../components/ProductPrice/ProductPrice";
+import ProductInstallment from "../../components/ProductInstallment/ProductInstallment";
 
 const ProductDetail = () => {
-  return (<>
-    <ChooseSize />
-    <AddPack/></>) 
-    
-  };
-  
-  export default ProductDetail;
-  
+  return (
+    <>
+      <Navbar />
+      <ProductName text="Vestido Transpasse Bow" />
+      <ProductPrice price="199,90" size="large" />
+      <ProductInstallment quantity="3" value="68,90" />
+      <ChooseSize />
+      <AddPack />
+    </>
+  );
+};
+
+export default ProductDetail;
