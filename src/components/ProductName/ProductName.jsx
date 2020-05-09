9c206930892ab822c091ea62468867d4672d3bdf
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-05-09 12:24:03 -0300

    tarefa: cria prop para passar tamanho da font ao ProductPrice

--- src/components/ProductName/ProductName.jsx
+++ src/components/ProductName/ProductName.jsx
@@ -3,12 +3,18 @@ import PropTypes from "prop-types";
 
 import "./ProductName.scss";
 
-const ProductName = ({ text }) => {
-  return <h3 className="product-title">{text}</h3>;
+const ProductName = ({ text, size }) => {
+  const className = `product-title product-title--${size}`;
+  return <h3 className={className}>{text}</h3>;
+};
+
+ProductName.defaultProps = {
+  size: "medium",
 };
 
 ProductName.propTypes = {
   text: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(["small", "medium", "large"]),
 };
 
 export default ProductName;
