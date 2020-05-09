97a02550199a01c012691529c4520e0adb783f77
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-08 23:44:06 -0300

    feature: cria componente ProductInstallment

---
+++ src/components/ProductInstallment/ProductInstallment.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./ProductInstallment.scss";
+
+const ProductInstallment = ({ quantity, value }) => {
+  return (
+    <>
+      <span className="product__installment">
+        em até {quantity}x R${value}
+      </span>
+    </>
+  );
+};
+
+ProductInstallment.propTypes = {
+  quantity: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
+
+export default ProductInstallment;
