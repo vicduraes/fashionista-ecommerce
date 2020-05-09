2d627eed961e7f419b9d44ffcec8c7caed0a3bf7
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-05-09 16:28:37 -0300

    tarefa: estrutura page de datalhes

--- src/components/ProductPrice/ProductPrice.jsx
+++ src/components/ProductPrice/ProductPrice.jsx
@@ -3,14 +3,22 @@ import PropTypes from "prop-types";
 import "./ProductPrice.scss";
 
 const ProductPrice = ({ price, size }) => {
-  var className = 'product__price--' + size ;
-  return <span className={className}> R${price}</span>
-  };
+  const className = `product__price--${size}`;
+  return (
+    <span className={className}>
+      R$
+      {price}
+    </span>
+  );
+};
+
+ProductPrice.defaultProps = {
+  size: "medium",
+};
 
 ProductPrice.propTypes = {
   price: PropTypes.number.isRequired,
-  size:  PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
 };
 
 export default ProductPrice;
-
--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -6,15 +6,30 @@ import ProductName from "../../components/ProductName/ProductName";
 import ProductPrice from "../../components/ProductPrice/ProductPrice";
 import ProductInstallment from "../../components/ProductInstallment/ProductInstallment";
 
+import "./ProductDetail.scss";
+
+const imgDefault = "http://via.placeholder.com/470x680";
+
 const ProductDetail = () => {
   return (
     <>
-      <Navbar />
-      <ProductName text="Vestido Transpasse Bow" />
-      <ProductPrice price="199,90" size="large" />
-      <ProductInstallment quantity="3" value="68,90" />
-      <ChooseSize />
-      <AddPack />
+      <div className="product-detail">
+        <Navbar />
+        <div className="container">
+          <div className="detail">
+            <figure className="detail__img">
+              <img src={imgDefault} alt="vestido transpasse" />
+            </figure>
+            <div className="detail__infos">
+              <ProductName text="Vestido Transpasse Bow" />
+              <ProductPrice price="199,90" size="large" />
+              <ProductInstallment quantity="3" value="68,90" />
+              <ChooseSize />
+              <AddPack />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
