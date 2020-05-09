6ebbaa226721cad6d5546ee1c69861ba63aca70a
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-09 17:20:44 -0300

    feature: cria componente detalhe

---
+++ src/components/Detail/Detail.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Detail.scss";
+
+import Navbar from "../Navbar/Navbar";
+import ChooseSize from "../ChooseSize/ChooseSize";
+import AddPack from "../AddPack/AddPack";
+import ProductName from "../ProductName/ProductName";
+import ProductPrice from "../ProductPrice/ProductPrice";
+import ProductInstallment from "../ProductInstallment/ProductInstallment";
+
+import imgDefault from "../../assets/images/img-default.svg";
+
+const Detail = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="detail">
+        <figure className="detail__img">
+          <img src={imgDefault} alt="vestido transpasse" />
+        </figure>
+        <div className="detail__info">
+          <ProductName text="Vestido Transpasse Bow" />
+          <div className="detail__prices">
+            <ProductPrice price="199,90" size="large" />
+            <ProductInstallment quantity="3" value="68,90" />
+          </div>
+          <ChooseSize />
+          <AddPack />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Detail;
