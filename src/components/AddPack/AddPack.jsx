b35fbc83d1f0bd7a18c97f14d6ebc5647b6bf45e
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-05-18 08:08:16 -0300

    ajuste da responsividade do card

--- src/components/AddPack/AddPack.jsx
+++ src/components/AddPack/AddPack.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+
 import "./AddPack.scss";
 
 const AddPack= ()=>{
     return(
         <div className="add-pack">
-        <button className= "add-pack__button" >Adicionar à sacola</button>
+            <button className= "add-pack__button" >Adicionar à sacola</button>
         </div>
-        )
-    }
-    
-    export default AddPack;
+    )
+}
+      
+export default AddPack;
