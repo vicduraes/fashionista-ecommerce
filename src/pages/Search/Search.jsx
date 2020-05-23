649a8e06edb36e172fd130f5046dc808f3b53d07
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-22 23:50:37 -0300

    funcionalidade: cria componente CardSearch

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../../components/Navbar/Navbar";
+import CardSearch from "../../components/CardSearch/CardSearch";
+
+const Search = () => {
+  return (
+    <div className="product-detail">
+      <>
+        <Navbar />
+        <CardSearch />
+      </>
+    </div>
+  );
+};
+
+export default Search;
