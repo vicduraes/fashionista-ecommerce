a5c571d6ea8cfdb968e57032327ba9f90b42254a
vicduraes <duraes.vic@gmail.com>
2020-05-22 21:49:40 -0300

    funcionalidade: cria modal

--- src/components/SearchButton/SearchButton.jsx
+++ src/components/SearchButton/SearchButton.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Modal from "../Modal/Modal";
 
 import "./SearchButton.scss";
 
 const SearchButton = () => {
+  const [show, setShow] = useState(false);
+
+  const openModal = () => setShow(true);
+  const closeModal = () => setShow(false);
+
   return (
-    <button className="search-button">
+    <>
+      <button className="search-button" onClick={openModal}>
         <FontAwesomeIcon icon={faSearch} />
-    </button>
+      </button>
+      <Modal closeModal={closeModal} show={show} />
+    </>
   );
 };
 
