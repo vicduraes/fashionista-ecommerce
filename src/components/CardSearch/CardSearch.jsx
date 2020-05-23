920ffb6638ecc7bb2a44caf4a0023bfeb1f6e6c7
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-05-22 23:50:37 -0300

    funcionalidade: cria componente CardSearch

---
+++ src/components/CardSearch/CardSearch.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./CardSearch.scss";
+
+import ProductImg from "../ProductImg/ProductImg";
+import ProductName from "../ProductName/ProductName";
+import ProductPrice from "../ProductPrice/ProductPrice";
+import ProductInstallment from "../ProductInstallment/ProductInstallment";
+import imgDefault from "../../assets/images/img-default.jpeg";
+
+const CardSearch = () => {
+  return (
+    <>
+      <div className="card-search">
+        <div className="card-search__img">
+          <ProductImg src={imgDefault} imgDesciption="Vestido Transpasse Bow" />
+        </div>
+        <div className="card-search__description">
+          <ProductName text="Vestido Transpasse Bow" size="medium" />
+        </div>
+        <div className="card-search__prices">
+          <ProductPrice price="199,00" size="medium" />
+          <ProductInstallment text="3x R$53,90" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardSearch;
