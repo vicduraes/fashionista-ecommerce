238653c009d36aa1e9c64557c36b9d93bd5852be
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-05-28 22:02:43 -0300

    tarefa: finaliza componente de CardShop

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-
 import CardSearch from "../../components/CardSearch/CardSearch";
+import CardShop from "../../components/CardShop/CardShop";
 import NavbarSecondary from "../../components/NavbarSecondary/NavbarSecondary";
 import Subtotal from "../../components/Subtotal/Subtotal";
 
@@ -9,9 +9,9 @@ const Search = () => {
   return (
     <div className="product-detail">
       <>
-        <NavbarSecondary total="2"/>
-        <CardSearch />
-        <Subtotal value="R$ 480,00"/>
+        <NavbarSecondary total="2" />
+        <CardShop />
+        <Subtotal value="R$ 480,00" />
       </>
     </div>
   );
