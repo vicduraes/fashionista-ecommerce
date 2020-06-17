36a3484c59403da2a9f84ec383258297f8c391da
vicduraes <duraes.vic@gmail.com>
2020-06-17 18:35:16 -0300

    tarefa: desabilita botao sem tamanho selecionado

--- src/components/AddPack/AddPack.jsx
+++ src/components/AddPack/AddPack.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import classnames from "classnames";
 
 import "./AddPack.scss";
 
-const AddPack = () => {
+const AddPack = (props) => {
+  const { selectedSize } = props;
+
+  const disabledButton = () => {
+    let bool = true;
+    if (selectedSize !== "") {
+      bool = false;
+    }
+    return bool;
+  };
+
   return (
     <div className="add-pack">
-      <button type="button" className="add-pack__button">
+      <button
+        type="button"
+        className={classnames("add-pack__button", {
+          "add-pack__button--disabled": selectedSize === "",
+        })}
+        disabled={disabledButton()}
+      >
         Adicionar à sacola
       </button>
     </div>
--- src/components/ChooseSize/ChooseSize.jsx
+++ src/components/ChooseSize/ChooseSize.jsx
@@ -3,42 +3,40 @@ import classnames from "classnames";
 
 import "./ChooseSize.scss";
 
-const ChooseSize = (props) => {  
-  const { sizes } = props;
-  
-  const [idButton, setIdButton] = useState('');  
-  
-  const handleClickButton = (id) => {    
+const ChooseSize = (props) => {
+  const { sizes, setSelectedSize } = props;
+
+  const [idButton, setIdButton] = useState("");
+
+  const handleClickButton = (id) => {
+    setSelectedSize(id);
     return setIdButton(id);
   };
-  
-  
-  
+
   return (
     <div className="field-choose-size">
       <span>Escolha o tamanho:</span>
       <div className="field-choose-size__button">
         {sizes &&
           sizes.map(
-          (size) =>
-            size.available && (
-              <button 
-                key={size.sku}
-                type="button"
-                className={classnames("field-choose-size__size-button", {
-                  "field-choose-size__size-button--click": idButton === size.sku
-                })}
-                onClick={ () => handleClickButton(size.sku)}
+            (size) =>
+              size.available && (
+                <button
+                  key={size.sku}
+                  type="button"
+                  className={classnames("field-choose-size__size-button", {
+                    "field-choose-size__size-button--click":
+                      idButton === size.sku,
+                  })}
+                  onClick={() => handleClickButton(size.sku)}
                 >
-                {size.size}
-              </button> 
-        ))}
-      
-      
+                  {size.size}
+                </button>
+              )
+          )}
       </div>
-      </div>
-      );
+    </div>
+  );
 };
-    
-    export default ChooseSize;
-    
+
+export default ChooseSize;
