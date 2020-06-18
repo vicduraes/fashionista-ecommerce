c8ed0ad3c83e663c55c63fb57c2040803c2ce1e4
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-06-17 23:12:11 -0300

    tarefa: ajustes com botão de search

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
-// import BackArrowIcon from "../BackArrowIcon/BackArrowIcon";
 import SearchBar from "../SearchBar/SearchBar";
 import Subtotal from "../Subtotal/Subtotal";
-import CardSearch from "../CardSearch/CardSearch";
 import NavbarSecondary from "../NavbarSecondary/NavbarSecondary";
-import CardShop from "../CardShop/CardShop";
 import "./Modal.scss";
 
 const Modal = (props) => {
   const { show, closeModal } = props;
-  const product = {
-    actual_price: "R$ 199,90",
-    code_color: "20002605_613",
-    color: "TAPEÇARIA",
-    color_slug: "tapecaria",
-    discount_percentage: "",
-    image:
-      "https://viniciusvinna.netlify.app/assets/api-fashionista/20002605_615_catalog_1.jpg",
-    installments: "3x R$ 66,63",
-    name: "VESTIDO TRANSPASSE BOW",
-    on_sale: false,
-    regular_price: "R$ 199,90",
-  };
 
   const modalComponent = (
     <>
