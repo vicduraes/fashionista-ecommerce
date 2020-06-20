6b827a316395ac18e497cdfda756da39991d2d17
Rafaela Guimarães <rafaelalg.santos@gmail.com>
2020-06-20 00:50:13 -0300

    correção: exibe mensagem de erro da API

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,13 @@
+const handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+};
+
 const getAPI = () =>
   fetch("https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog")
-    .then((response) => response.json())
+    .then(handleErrors)
     .then((data) => data);
+
 export default getAPI;
