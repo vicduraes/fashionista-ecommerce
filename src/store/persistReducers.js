7c440febbad0eb583efd541ce475f5971f3c153a
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-06-26 22:15:01 -0300

    tarefa: add immer como dependencia

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from "redux-persist";
 export default (reducers) => {
   const persistedReducer = persistReducer(
     {
-      key: "fashionista",
+      key: "amora",
       storage,
       whitelist: ["cart", "products"],
     },
