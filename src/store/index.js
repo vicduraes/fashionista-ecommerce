14c7f24b732e95feffb09a89611fed9ec048f5ca
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-06-26 22:15:01 -0300

    tarefa: add immer como dependencia

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,3 @@
-// import { createStore, applyMiddleware } from "redux";
-// import createSagaMiddleware from "redux-saga";
-// import { composeWithDevTools } from "redux-devtools-extension";
-
-// import rootReducer from "./reducers";
-// import rootSaga from "./sagas";
-
-// const sagaMiddleware = createSagaMiddleware();
-
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(sagaMiddleware))
-// );
-
-// sagaMiddleware.run(rootSaga);
-
-// export default store;
-
 import { persistStore } from "redux-persist";
 import createSagaMiddleware from "redux-saga";
 import createStore from "./createStore";
